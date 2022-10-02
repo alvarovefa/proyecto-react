7bd7da23e1d1ff86c65c8e9b87f3f740b107d938
Alvaro Vergara <alvaro.vergara.f@gmail.com>
2022-10-02 00:10:55 -0300

    Agregando componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import  React from 'react';
+import {TodoCounter}  from  './TodoCounter';
+import {TodoSearch} from './TodoSearch';
+import {TodoList} from './TodoList';
+import {TodoItem} from './TodoItem';
+import {CreateTodoButton} from './CreateTodoButton';
 
 function App() {
+  const todos = [
+    { text: 'Picar cebolla', completed:false},
+    { text: 'Estudiar React', completed:false},
+    { text: 'Llorar', completed:false},
+  ]
+  //cada componente de react sólo soporta que se le
+  //envíe 1 componente
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //react sólo necesita que le enviemos 1 etiqueta por componente
+  //es como un div invisible al html, cuenta como la etiqueta por componente
+    <React.Fragment>
+
+    <TodoCounter />
+        
+    <TodoSearch />
+        <input placeholder="Escriba aquí"/>
+    <TodoList>
+      {todos.map(todo => (
+        // Siempre que quiero iterar una Array
+        // con React debo asignar una key al componente que los va a mostrar
+        <TodoItem key={todo.text} text={todo.text} />
+      ))}
+    </TodoList>
+    <CreateTodoButton />
+    </React.Fragment>
+    
+  )
+
+
 }
 
 export default App;
---
+++ src/CreateTodoButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CreateTodoButton(props) {
+    return (
+        <button>Agregar</button>
+    );
+}
+
+export {CreateTodoButton};
