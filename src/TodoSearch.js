5595e4fe058f21e7c8bebfcadc3d67ec47b51267
Alvaro Vergara <alvaro.vergara.f@gmail.com>
2022-10-02 03:27:57 -0300

    Contando tareas y buscando

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({searchValue, setSearchValue}) {
+
     const onSearValueChange = (event) => {
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
   return (
     <input 
         className="TodoSearch" 
-        placeholder="Cebolla" 
+        placeholder="Escriba aquí"
+        value={searchValue} 
         onChange={onSearValueChange}    
-    
+
     />
   );
+
+
+
 }
 
 export { TodoSearch };
